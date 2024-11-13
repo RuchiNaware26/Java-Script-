2ecbcf8732f31f2dc71c5c3255723cf41f279233
RuchiNaware26 <rnn@njit.edu>
2024-11-13 11:58:03 -0500

    lecture

---
+++ script.js
@@ -0,0 +1,82 @@
+var age = 20;
+
+var age; 
+
+age = 100;
+
+
+let username = "Harry";
+
+username = "Potter";
+
+let FirstName = "Ruchi";
+
+const ValuePi = 3.14;
+
+let grade = false;
+
+let house; 
+
+let house2 = null;
+
+let age2 = 49659865893269236952365832687368746739865626236938268365765558365816036n;
+
+//This is a single line comment//
+
+let a = 10;
+
+let b = 5;
+
+b = b + 5;
+
+a = a - 5;
+
+let c = 5;
+
+let d = 12;
+
+let e = true;
+
+let f = false;
+
+
+
+let house3;
+
+let number = 100;
+
+if(number != 50){
+    house3 = "The numbers are same";  
+    console.log(house3);
+}
+
+else{
+    house3 = "The numbers are not same";
+    console.log(house3)
+}
+
+
+    
+    
+
+
+
+// b == 5; ==> b = b + 5;
+// b *= 5; ==> b = b * 5;
+console.log(age);
+console.log(username);
+console.log(FirstName);
+const valuePi = 3.14;
+console.log(valuePi);
+console.log(grade);
+console.log(house);
+console.log(null);
+console.log(age2);
+console.log(a+b);
+console.log(c*d);
+console.log(a == b);
+console.log( a != b);
+console.log( a === b);
+console.log(e && f);
+console.log(e || f);
+console.log(!e);
